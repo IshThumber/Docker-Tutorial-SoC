adc39448d502fca9a25baf8c0b38d8c16e8ecaf0
Ish Thumber <ishthumber343@gmail.com>
2022-12-26 18:30:45 +0530

    container 1 Containing node server

---
+++ container 1/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    console.log('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+
+    
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+});
